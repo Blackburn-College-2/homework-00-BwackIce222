93935c03ed733d7d68848f5ea437a2c1bb1c46f8
montreal_thomas <montreal.thomas@blackburn.edu>
2020-01-17 14:45:45 -0600

    minor revisions including comments and time elapsed

--- src/guessinggame/GuessingGame.java
+++ src/guessinggame/GuessingGame.java
@@ -16,6 +16,11 @@ public class GuessingGame {
     /**
      * @param args the command line arguments
      */
+    /*This the main method where the majority of the work for the guessing 
+    game is done.
+    //This is a guessing game that lets the user guess one of three secret 
+    meals. 
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] meals = new String[3];
@@ -32,28 +37,41 @@ public class GuessingGame {
         System.out.println("shrimp");
         System.out.println("tacos");
         System.out.println("sushi");
-        
-            System.out.print("Geuss the secret meal:");
-            guess = input.nextLine();
-            while (playing > 0) {
+
+        System.out.print("Geuss the secret meal:");
+        guess = input.nextLine();
+        // This while loop allows the user to guess the secret food a maximum 
+        //of two times unless they get it right the first time. If the user does
+        //not guess the meal correctly in two tries the game will stop and they 
+        //will lose. The user will also lose by default if they enter an option 
+        //other than the provided foods.
+        while (playing > 0) {
             if (guess.equalsIgnoreCase(secretFood)) {
                 System.out.println("Correct! You win!");
                 playing = playing - 3;
-            } else if((guess.equalsIgnoreCase("sushi")||guess.equalsIgnoreCase("shrimp")||guess.equalsIgnoreCase("tacos")) != true) {
+            } else if ((guess.equalsIgnoreCase("sushi")
+                    || guess.equalsIgnoreCase("shrimp")
+                    || guess.equalsIgnoreCase("tacos")) != true) {
                 playing = 0;
-            }else{
+            } else {
                 System.out.println("Wrong!");
                 playing = playing - 1;
-                if(playing>=1){
-                System.out.println("Guess the secret meal:");
-                guess = input.nextLine();
-            }
+                if (playing >= 1) {
+                    System.out.println("Guess the secret meal:");
+                    guess = input.nextLine();
+                }
+                if(guess.equalsIgnoreCase(guess) && playing<1){
+                    System.out.println("You already guess that!");
+                }
             }
 
         }
-        if(playing==0){
+        if (playing == 0) {
             System.out.println("Sorry you lose :( ");
         }
     }
 
 }
+/*
+Time estimated: 1 hour, Time taken: 28 min
+*/
